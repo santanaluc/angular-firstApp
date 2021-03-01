085369f6117f4264c69129f6cff859b11b7b3af2
santanaluc <lucas.s.s.silva@hotmail.com>
2021-03-01 11:46:53 -0300

    Initial commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,17 @@
+import { Component, Inject, OnInit } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+
+@Component({
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"]
+})
+export class AboutComponent implements OnInit {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  goToUrl(): void {
+    this.document.location.href = "https://github.com/santanaluc";
+  }
+
+  ngOnInit() {}
+}
